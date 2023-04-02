ef5c30878e98fb2a3d1de1954620b5284824fd21
mrlrcn <m.alarconarbona@gmail.com>
2023-04-02 21:37:59 +0200

    :lipstick: fixin woocommerce as possible w fse

--- wp-content/themes/projectbeta/front-page.php
+++
@@ -1,3 +0,0 @@
-<?php get_header(); the_post(); ?>
-front-page
-<?php get_footer(); ?>
--- wp-content/themes/projectbeta/functions.php
+++ wp-content/themes/projectbeta/functions.php
@@ -1,17 +1,41 @@
 <?php
-// CARGAR JS Y CSS
-    function my_scripts_and_css() {
-        //  REMOVE GUTENBERG CSS
-        wp_dequeue_style ( 'wp-block-library' );
-        wp_dequeue_style ( 'wp-block-library-theme' );
-        wp_dequeue_style ( 'wc-blocks-style' );
-        if ( !is_admin() ) {
-            //if ( is_page ( 'Contact' )) {
-                //wp_enqueue_script ( 'js-main', get_stylesheet_directory_uri() . '/js/main.js', '', '', true );
-            //}
-            // wp_enqueue_style ( 'css-main', get_stylesheet_uri() );
-            wp_enqueue_style ( 'css-main', get_stylesheet_uri(), '', filemtime ( get_template_directory() . '/style.css' ) );
+
+//  CARGAR MIS ESTILOS
+
+    function my_scripts_and_styles()
+    {
+        if ( ! is_admin() )
+        {            
+            wp_enqueue_style ( 'theme-styles', get_stylesheet_uri(), '', filemtime ( get_template_directory() . '/style.css' ) );
         }
     }
-    add_action ( 'wp_enqueue_scripts', 'my_scripts_and_css', 100 );
+
+    add_action ( 'wp_enqueue_scripts', 'my_scripts_and_styles' );
+
+//  THEME SETUP
+
+    function my_theme_setup()
+    {
+        remove_theme_support ( 'core-block-patterns' );
+    }
+
+    add_action ( 'after_setup_theme', 'my_theme_setup' );
+
+
+
+//  GUTENBERG: LOCKING CONTENT FOR NON ADMINS
+
+    function my_gutenberg_locks ( $settings, $context )
+    {
+        if ( ! current_user_can ( 'activate_plugins' ) )
+        {
+            $settings[ 'canLockBlocks' ] = false;
+            $settings[ 'codeEditingEnabled' ] = false;
+        }
+
+        return $settings;
+    }
+
+    add_filter ( 'block_editor_settings_all', 'my_gutenberg_locks', 10, 2 );
+
 ?>
